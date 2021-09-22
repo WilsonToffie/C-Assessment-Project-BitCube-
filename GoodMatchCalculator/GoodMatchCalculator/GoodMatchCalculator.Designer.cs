c95428555fdd57d433e002d03d4fab82c2ff016b
WilsonToffie <88487501+WilsonToffie@users.noreply.github.com>
2021-09-22 22:15:31 +0200

    first commit

---
+++ GoodMatchCalculator/GoodMatchCalculator/GoodMatchCalculator.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace GoodMatchCalculator
+{
+    partial class GoodMatchCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlayerTwo = new System.Windows.Forms.TextBox();
+            this.txtPlayerOne = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnLocateFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.pnlMatch = new System.Windows.Forms.Panel();
+            this.lblCalculated = new System.Windows.Forms.Label();
+            this.pbxPercentage1 = new System.Windows.Forms.PictureBox();
+            this.lbxAllMatches = new System.Windows.Forms.ListBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlMatch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPercentage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPlayerTwo
+            // 
+            this.txtPlayerTwo.Location = new System.Drawing.Point(12, 102);
+            this.txtPlayerTwo.Name = "txtPlayerTwo";
+            this.txtPlayerTwo.Size = new System.Drawing.Size(100, 23);
+            this.txtPlayerTwo.TabIndex = 0;
+            // 
+            // txtPlayerOne
+            // 
+            this.txtPlayerOne.Location = new System.Drawing.Point(12, 44);
+            this.txtPlayerOne.Name = "txtPlayerOne";
+            this.txtPlayerOne.Size = new System.Drawing.Size(100, 23);
+            this.txtPlayerOne.TabIndex = 1;
+            this.txtPlayerOne.TextChanged += new System.EventHandler(this.txtPlayerOne_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter player one\'s name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter player two\'s name";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 144);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnLocateFile
+            // 
+            this.btnLocateFile.Location = new System.Drawing.Point(698, 202);
+            this.btnLocateFile.Name = "btnLocateFile";
+            this.btnLocateFile.Size = new System.Drawing.Size(90, 23);
+            this.btnLocateFile.TabIndex = 5;
+            this.btnLocateFile.Text = "Select csv file";
+            this.btnLocateFile.UseVisualStyleBackColor = true;
+            this.btnLocateFile.Click += new System.EventHandler(this.btnLocateFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(444, 202);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(248, 23);
+            this.txtFilePath.TabIndex = 6;
+            // 
+            // pnlMatch
+            // 
+            this.pnlMatch.Controls.Add(this.lblCalculated);
+            this.pnlMatch.Controls.Add(this.pbxPercentage1);
+            this.pnlMatch.Location = new System.Drawing.Point(151, 26);
+            this.pnlMatch.Name = "pnlMatch";
+            this.pnlMatch.Size = new System.Drawing.Size(287, 199);
+            this.pnlMatch.TabIndex = 7;
+            // 
+            // lblCalculated
+            // 
+            this.lblCalculated.AutoSize = true;
+            this.lblCalculated.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCalculated.Location = new System.Drawing.Point(10, 63);
+            this.lblCalculated.Name = "lblCalculated";
+            this.lblCalculated.Size = new System.Drawing.Size(72, 30);
+            this.lblCalculated.TabIndex = 1;
+            this.lblCalculated.Text = "label3";
+            // 
+            // pbxPercentage1
+            // 
+            this.pbxPercentage1.Image = global::GoodMatchCalculator.Properties.Resources._0;
+            this.pbxPercentage1.Location = new System.Drawing.Point(10, 18);
+            this.pbxPercentage1.Name = "pbxPercentage1";
+            this.pbxPercentage1.Size = new System.Drawing.Size(267, 42);
+            this.pbxPercentage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPercentage1.TabIndex = 0;
+            this.pbxPercentage1.TabStop = false;
+            // 
+            // lbxAllMatches
+            // 
+            this.lbxAllMatches.FormattingEnabled = true;
+            this.lbxAllMatches.ItemHeight = 15;
+            this.lbxAllMatches.Location = new System.Drawing.Point(444, 26);
+            this.lbxAllMatches.Name = "lbxAllMatches";
+            this.lbxAllMatches.Size = new System.Drawing.Size(344, 169);
+            this.lbxAllMatches.TabIndex = 8;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(12, 8);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(186, 15);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "Incorrect input, please try again.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(444, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 10;
+            // 
+            // GoodMatchCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 237);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lbxAllMatches);
+            this.Controls.Add(this.pnlMatch);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnLocateFile);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlayerOne);
+            this.Controls.Add(this.txtPlayerTwo);
+            this.Name = "GoodMatchCalculator";
+            this.Text = "GoodMatch";
+            this.pnlMatch.ResumeLayout(false);
+            this.pnlMatch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPercentage1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlayerTwo;
+        private System.Windows.Forms.TextBox txtPlayerOne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnLocateFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Panel pnlMatch;
+        private System.Windows.Forms.ListBox lbxAllMatches;
+        private System.Windows.Forms.Label lblError;
+        public System.Windows.Forms.PictureBox pbxPercentage1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCalculated;
+    }
+}
+
---
+++ GoodMatchCalculator/GoodMatchCalculator/GoodMatchCalculator.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodMatchCalculator
+{
+    public partial class GoodMatchCalculator : Form
+    {
+        string player1, player2, path;
+        int matchPerc;
+        List<string> female = new List<string>();
+        List<string> male = new List<string>();
+        List<string> matches = new List<string>();
+        static List<Image> imgList = new List<Image>();
+
+        public GoodMatchCalculator()
+        {
+            InitializeComponent();
+  
+            lblError.Visible = false;
+            txtFilePath.ReadOnly = true;
+            lblCalculated.Visible = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            player1 = txtPlayerOne.Text.ToLower();
+            player2 = txtPlayerTwo.Text.ToLower();
+
+            //Checks if the input is only alphanumeric characters
+            if (!Regex.IsMatch(player1, @"^[a-zA-Z]+$"))
+            {
+                txtPlayerOne.Text = "";
+                txtPlayerTwo.Text = "";
+                lblError.Visible = true;
+                Log(player1);
+            }
+            else if (!Regex.IsMatch(player2, @"^[a-zA-Z]+$"))
+            {
+                txtPlayerOne.Text = "";
+                txtPlayerTwo.Text = "";
+                lblError.Visible = true;
+                Log(player2);
+            }
+            else
+            {
+                int percentage = int.Parse(CalculateMatch(player1, player2));// returns a match percentage
+
+                AddImages();
+                //Ads images to a list and then those images correspond to the images according to the match percentage
+                if (percentage <= 10)
+                    pbxPercentage1.Image = imgList[0];
+                else if (percentage <= 20)
+                    pbxPercentage1.Image = imgList[1];
+                else if (percentage <= 30)
+                    pbxPercentage1.Image = imgList[2];
+                else if (percentage <= 40)
+                    pbxPercentage1.Image = imgList[3];
+                else if (percentage <= 50)
+                    pbxPercentage1.Image = imgList[4];
+                else if (percentage <= 60)
+                    pbxPercentage1.Image = imgList[5];
+                else if (percentage <= 70)
+                    pbxPercentage1.Image = imgList[6];
+                else if (percentage < 80)
+                    pbxPercentage1.Image = imgList[7];
+                //Displays a green label if its a good match
+                if (percentage >= 80)
+                {
+                    pbxPercentage1.Image = imgList[12];
+                    lblCalculated.Text = percentage + "% good match!";
+                    lblCalculated.ForeColor = Color.Green;
+                    lblCalculated.Visible = true;
+                }
+                else//Displays a normal black lable for matches below 80% 
+                {
+                    lblCalculated.Text = percentage + "% match";
+                    lblCalculated.ForeColor = Color.Black;
+                    lblCalculated.Visible = true;
+                }
+
+            }
+
+        }
+
+        private void txtPlayerOne_TextChanged(object sender, EventArgs e)
+        {//After user has read that incorrect input was given and goes to start typing again, the error message dissapears.
+            lblError.Visible = false;
+        }
+
+        private void btnLocateFile_Click(object sender, EventArgs e)
+        {//Clears all the lists soon to be populated
+            matches.Clear();
+            male.Clear();
+            female.Clear();
+
+            lbxAllMatches.Items.Clear();
+            lbxAllMatches.Items.Add("Player 1 \t \tPlayer 2 \tPercentage");
+            lbxAllMatches.Items.Add("------------------------------------------------------");
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            //Only displays files in the correct csv format to avoid errors
+            ofd.Filter = "CSV Files (*.csv)|*.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+                path = ofd.FileName;
+
+            txtFilePath.Text = path;
+
+            StreamReader reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {//Takes each line and assigns each name to a list according to gender using a split to identify 'm' and 'f' for male and female
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                if (values[1] == "m")
+                {
+                    if (!male.Contains(values[0]))
+                        male.Add(values[0]);
+                }
+                else
+                {
+                    if (!female.Contains(values[0]))
+                        female.Add(values[0]);
+                }
+            }
+            //Takes and mathes each males with every female and adds it to a list with the match percentage
+            for (int i = 0; i < male.Count(); i++)
+            {
+                for (int j = 0; j < female.Count(); j++)
+                {
+                    matchPerc = int.Parse(CalculateMatch(male[i], female[j]));
+                    if (matchPerc >= 80)
+                        matches.Add(male[i] + " \tmatches \t" + female[j] + " \t" + matchPerc + " %, good match.");
+                    else
+                        matches.Add(male[i] + " \tmatches \t" + female[j] + " \t" + matchPerc + " %");
+                }
+            }
+            
+            matches = Sort(matches);
+            SaveToFile(matches);
+
+            foreach (string match in matches)
+            {//Populates listbox with all the different possible matches
+                lbxAllMatches.Items.Add(match);
+            }
+        }
+        //Calculates match percentage
+        static string CalculateMatch(string player1, string player2)
+        {
+            string s = player1 + "matches" + player2,
+                    occurences = "";
+            int count = 0;
+            int length = s.Length;
+
+            char first;
+            //Takes each character and checks the count of each in the sentence then removes all the corresponding characters from the sentence using substring() untill there are none left
+            for (int i = 0; i < length; i++)
+            {
+                if (s.Length != 0)
+                    first = s[0];
+                else
+                    break;
+
+                foreach (char letter in s)
+                {
+                    if (letter == first)
+                    {
+                        s = s.Remove(s.IndexOf(letter), 1);
+                        count++;
+                    }
+                }
+                occurences += count;
+                count = 0;
+            }
+
+
+            int head;
+            int tail;
+            string temp = occurences;
+            string final = "";
+            //Simplifies the character occurrences until only 2 digits are left.
+            while (temp.Length != 0)
+            {
+                if (temp.Length % 2 != 0)
+                {
+                    if (temp.Length == 1)
+                    {
+                        final += temp;
+
+                        if (final.Length == 2)
+                            break;
+
+                        temp = final;
+                        final = "";
+                    }
+                    else
+                    {
+                        head = 0;
+                        tail = temp.Length - 1;
+                        final += int.Parse(temp[head].ToString()) + int.Parse(temp[tail].ToString());
+                        temp = temp.Substring(head + 1, tail - 1);
+                    }
+                }
+                else
+                {
+                    head = 0;
+                    tail = temp.Length - 1;
+                    final += int.Parse(temp[head].ToString()) + int.Parse(temp[tail].ToString());
+                    temp = temp.Substring(head + 1, tail - 1);
+
+                    if (final.Length == 2 && temp == "")
+                        break;
+
+                    if (temp.Length == 0)
+                    {
+                        temp = final;
+                        final = "";
+                    }
+                }
+            }
+
+            return final;
+        }
+
+        public static List<string> Sort(List<string> input)
+        {//using split, this method sorts the percentages in descending fashion then by the first names alphabetically and then by the second names
+            var ordered = input.Select(s => new { Str = s, Split = s.Split(' ') })
+                        .OrderByDescending(x => int.Parse(x.Split[3]))
+                        .ThenBy(x => x.Split[0])
+                        .ThenBy(x => x.Split[2])
+                        .Select(x => x.Str)
+                        .ToList();
+
+            return ordered;
+        }
+
+        public static void SaveToFile(List<string> input)
+        {//Saves the csv loaded input onto a textfile
+            StreamWriter str = new StreamWriter("output.txt");
+
+            foreach (String s in input)
+                str.WriteLine(s);
+
+            str.Close();
+        }
+
+        public static void Log(string input)
+        {//Stores and keeps track of all incorrect inputs and what the actuall input is that was given
+            using (StreamWriter str = File.AppendText("log.txt"))
+            {
+                str.WriteLine($"{DateTime.Now} : Incorrect format entered :: {input}");
+                str.Close();
+            }
+        }
+
+        public static void AddImages()
+        {
+            imgList.Add(Properties.Resources._0);
+            imgList.Add(Properties.Resources._1);
+            imgList.Add(Properties.Resources._2);
+            imgList.Add(Properties.Resources._3);
+            imgList.Add(Properties.Resources._4);
+            imgList.Add(Properties.Resources._5);
+            imgList.Add(Properties.Resources._6);
+            imgList.Add(Properties.Resources._7);
+            imgList.Add(Properties.Resources._8);
+            imgList.Add(Properties.Resources._9);
+            imgList.Add(Properties.Resources._10);
+            imgList.Add(Properties.Resources._11);
+            imgList.Add(Properties.Resources._12);
+            imgList.Add(Properties.Resources._13);
+        }
+    }
+}
